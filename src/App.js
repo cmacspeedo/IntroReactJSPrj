c94ec27bb798e76509c7aaf3cbd88d47905f2193
cmacspeedo <cmacspeedo101@gmail.com>
2023-03-28 19:04:13 -0600

    replaced code in app js and css with counter js and css code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  // handle increment of counter
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  // handle decrement of the counter
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div>
+        <button className="btn btn-3" onClick={handleDecrement}>
+          Minus 1
+        </button>
+        <button className="btn btn-2" onClick={handleIncrement}>
+          Add 1
+        </button>
+
+        <h3 className="counter">Count is {count}</h3>
+        <button
+          className="btn btn-3"
+          onClick={() => {
+            setCount(0);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Reset Counter
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Counter;
